dec3c3a6374fe74f368c80996c0df5c00d25ec19
EashenMahmud <eehssan7@gmail.com>
2023-08-22 11:22:50 +0600

    DUDSAA alumni first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MembershipComponent } from './membership/membership.component';
+import { BenefitComponent } from './benefit/benefit.component';
+import { EventComponent } from './event/event.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'membership', component: MembershipComponent},
+  { path: 'benefit', component: BenefitComponent},
+  { path: 'event', component: EventComponent},
+  { path: 'gallery', component: GalleryComponent},
+  { path: 'contact', component: ContactComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MembershipComponent } from './membership/membership.component';
+import { BenefitComponent } from './benefit/benefit.component';
+import { EventComponent } from './event/event.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    MembershipComponent,
+    BenefitComponent,
+    EventComponent,
+    GalleryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
